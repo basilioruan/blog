46065c455e6d678af8220c182760c7328019d6c2
Ruan Basilio <ruan_basilio@outlook.com>
2023-04-10 15:30:37 -0300

    feat: creating backend project

---
+++ backend/src/test/java/com/github/ruanbasilio/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ruanbasilio.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
